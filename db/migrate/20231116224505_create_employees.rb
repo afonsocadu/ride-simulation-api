859ac1b5a042e36f8f518db16a913458111fa907
Carlos Afonso <caafonso@deloitte.com>
2023-11-17 17:34:51 +0000

    Initial commit

---
+++ db/migrate/20231116224505_create_employees.rb
@@ -0,0 +1,11 @@
+class CreateEmployees < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.string :user_name
+      t.text :technologies
+      t.references :project, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
